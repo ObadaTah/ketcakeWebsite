3ecac49c8a23c06ded9ff1201624e958faeaafd8
ObadaTah <obadatahboub20@gmail.com>
2024-05-13 18:32:50 +0300

    sadf

---
+++ src/main/java/ps/ketcake/website/Config.java
@@ -0,0 +1,19 @@
+package ps.ketcake.website;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+}
--- src/main/java/ps/ketcake/website/Controllers/CustomersController.java
+++ src/main/java/ps/ketcake/website/Controllers/CustomersController.java
@@ -29,4 +29,9 @@ public class CustomersController {
     public String menu() {
         return "customers/menu";
     }
+
+    @GetMapping("")
+    public String index() {
+        return "";
+    }
 }
